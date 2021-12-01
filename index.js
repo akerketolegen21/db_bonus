0a860cebe33d9a797db93a73e12a4ab70a201ea3
akerketolegen21 <akerketolegen.at@gmail.com>
2021-12-02 01:16:24 +0600

    fetch request trial

--- index.js
+++ index.js
@@ -137,7 +137,6 @@ app.post("/findAVG/:id", async (req, res) => {
 })
 
 if(process.env.NODE_ENV === 'production'){
-    const path  =  require('path');
     app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
